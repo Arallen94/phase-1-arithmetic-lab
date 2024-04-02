174db87992b44a1005c76311569742c92a9c4c69
Blairallen <arallen94@hotmail.com>
2024-04-02 14:48:58 -0700

    Arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+const num1 = 1
+const num2 = 62
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 4;
+let num4 = 5;
+let mod = num3 % num4;
+
+const set = [5, 10, 20, 3, 7];
+let max = -Infinity;
+
+for (let i = 0; i < set.length; i++) {
+  if (set[i] > max) {
+    max = set[i];
+  }
+}
